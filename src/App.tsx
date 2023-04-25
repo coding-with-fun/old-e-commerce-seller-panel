2ed86c21c5b8076d8cd131fd2b52eaf3015cf9d8
Harrsh Patel <dev.harrsh@gmail.com>
2023-04-25 16:11:52 +0530

    Tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import _ from 'lodash';
 import { useContext, useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { SocketContext } from './context/socket';
