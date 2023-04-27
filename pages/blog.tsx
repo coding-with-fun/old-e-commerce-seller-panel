68002fd1b0437952eaca282570789e96374e522b
Harrsh Patel <dev.harrsh@gmail.com>
2023-04-27 15:58:41 +0530

    Added SSR auth

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,5 +1,31 @@
-import React from "react";
+import { GetServerSideProps } from "next";
+import { Session } from "next-auth";
+import { getSession } from "next-auth/react";
 
-export default function Blog() {
-  return <div>Blog</div>;
+const Blog = ({ data }: IProps) => {
+  return data.user ? (
+    <div>Authorized blogs</div>
+  ) : (
+    <div>Unauthorized blogs</div>
+  );
+};
+
+export default Blog;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+      data: {
+        ...session,
+      },
+    },
+  };
+};
+
+interface IProps {
+  session: Session;
+  data: Session;
 }
